9041ba9587d4601586095be2de407a45ee128b25
heeom <swimxdev@gmail.com>
2024-11-24 17:10:22 +0900

    feat : 결합도 개선 - 캡슐화

--- src/main/java/com/example/ch_01/Theater.java
+++ src/main/java/com/example/ch_01/Theater.java
@@ -10,18 +10,6 @@ public class Theater {
     }
 
     public void enter(Audience audience) {
-        // 초대장 있는 관람객
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            // 티켓 판매
-            Long fee = ticket.getFee();
-            audience.getBag().minusAmount(fee);
-            ticketSeller.getTicketOffice().plusAmount(fee);
-
-            audience.getBag().setTicket(ticket);
-        }
+        ticketSeller.sellTo(audience);
     }
 }
