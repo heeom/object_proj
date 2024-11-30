5945011ad5e08801097a32e92e1970aee3cd51b8
heeom <swimxdev@gmail.com>
2024-11-30 17:49:47 +0900

    feat : Movie 클래스에 정책 필드 추가

--- src/main/java/com/example/screen/Movie.java
+++ src/main/java/com/example/screen/Movie.java
@@ -6,7 +6,7 @@ public class Movie {
 
     private String title;
     private String runningTime;
-    private String subject;
-    private BigDecimal amount;
+    private BigDecimal fee;
+    private String discountPolicy;
 
 }
