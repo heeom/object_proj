0910ec96ddc878b81fb49571830ca8e5b57cd1ca
heeom <swimxdev@gmail.com>
2025-01-20 21:37:00 +0900

    feat : 시간 구간별 요금 할인 정책 구현

--- src/main/java/com/example/ch14/phone/policy/TimeOfDayDiscountPolicy.java
+++ src/main/java/com/example/ch14/phone/policy/TimeOfDayDiscountPolicy.java
@@ -1,6 +1,7 @@
 package com.example.ch14.phone.policy;
 
 import com.example.ch14.phone.domain.Call;
+import com.example.ch14.phone.domain.DateTimeInterval;
 
 import java.math.BigDecimal;
 import java.time.Duration;
@@ -17,7 +18,29 @@ public class TimeOfDayDiscountPolicy extends BasicRatePolicy {
 
     @Override
     BigDecimal calculateCallFee(Call call) {
+        BigDecimal amount = BigDecimal.ZERO;
+        call.splitByDay().forEach(interval -> {
+            for (int loop = 0; loop < starts.size(); loop++) {
+                BigDecimal charge = amounts.get(loop); // 구간별 통화 요금
+                long callTime = Duration.between(from(interval, starts.get(loop)), to(interval, ends.get(loop)))
+                        .getSeconds() / durations.get(loop).getSeconds(); // 구간별 통화 시간
+                // 구간별 통화 요금 계산
+                amount.add(charge.multiply(BigDecimal.valueOf(callTime)));
+            }
+        });
+        return amount;
+    }
 
-        return null;
+    private LocalTime from(DateTimeInterval interval, LocalTime from) {
+        return interval.getFrom().toLocalTime().isBefore(from) ?
+                from :
+                interval.getFrom().toLocalTime();
+
+    }
+
+    private LocalTime to(DateTimeInterval interval, LocalTime to) {
+        return interval.getTo().toLocalTime().isAfter(to) ?
+                to :
+                interval.getTo().toLocalTime();
     }
 }
